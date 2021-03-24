e2bc8f80728cb2caebeb503a1ea8af95252c7033
hmzugur <craxx67@gmail.com>
2021-03-24 20:02:39 +0300

    DetailView & detail.html düzenlendi

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, DetailView
 from .models import Post
 
+#__________________________________ IndexView(ListView)__________________________
 
 class IndexView(ListView):
     template_name = 'posts/index.html' #templates->posts->index.html
@@ -12,3 +13,18 @@ class IndexView(ListView):
         context = super(IndexView,self).get_context_data(**kwargs)
 
         return context
+
+
+#__________________________________ PostDetail(DetailView)__________________________
+
+class PostDetail(DetailView):
+    template_name='posts/detail.html'
+    model = Post
+    context_object_name = 'single'
+
+
+    def get_context_data(self, **kwargs):
+        context= super(PostDetail, self).get_context_data(**kwargs)
+
+        return context
+
