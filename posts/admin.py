c222d545bbb395b9485bfdea96b93995010f28e1
hmzugur <craxx67@gmail.com>
2021-03-24 05:39:30 +0300

    posts->models.py & admin.py ayarlandı

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from posts.models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class AdminPost(admin.ModelAdmin):
+    list_filter=['publishing_date']
+    list_display=['title','publishing_date','image']
+    list_display_links = ['title','publishing_date','image']
+    search_fields = ['title','content']
+
+
+    class Meta:
+        model =Post
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,33 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    publishing_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=True,blank=True, upload_to='resimler/')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)#ForeignKey -> 1'e çok yani, 1 kullanıcı bir sürü yazı ekleyebilir ilişkisi
+
+
+
+
+    def __str__(self):
+        return self.title
+
+
+
+
+"""
+   1 user -> Bir sürü yazısı olabilir, fakat 1 yazının(Aynı yazı) bir sürü useri olamaz :ForeignKey
+    1 etiket bir çok posta ait olabilir, aynı zamanda o yazıda bir çok etikete sahip olabilir:ManyToMany
+"""
+
+
+"""
+ForeignKey -> on_delete = models.CASCADE yapmalısın yani,bu kullanıcıyı sildin,bütün yazıları sil.
+default=1 -> id numarası 1 olan kullanıcı anlamına gelir. Var olan postlarınıza bir kullanıcı ataması yapar.
+
+
+"""
