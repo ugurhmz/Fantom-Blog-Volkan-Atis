4a29b6b9fc3e0c3a4884eb9bd6a2a7c94359e416
hmzugur <craxx67@gmail.com>
2021-03-24 06:06:34 +0300

    ListView düzenlendi, db'den postlar çekildi

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
+from .models import Post
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'posts/index.html' #templates->posts->index.html
+    model = Post
+    context_object_name = 'posts'  #key ->value gibi düşünebilirsin.
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IndexView,self).get_context_data(**kwargs)
+
+        return context
